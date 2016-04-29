fa7bd2fc0bcb002bd113ef18f161cf24a96b82ab
Carsten Könemann <cargath@gmail.com>
2016-04-29 09:53:41 +0200

    initialized Xcode project

---
+++ MarvelKit/MarvelKit/MarvelKit.h
@@ -0,0 +1,19 @@
+//
+//  MarvelKit.h
+//  MarvelKit
+//
+//  Created by Carsten Könemann on 29.04.16.
+//  Copyright © 2016 cargath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MarvelKit.
+FOUNDATION_EXPORT double MarvelKitVersionNumber;
+
+//! Project version string for MarvelKit.
+FOUNDATION_EXPORT const unsigned char MarvelKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarvelKit/PublicHeader.h>
+
+
