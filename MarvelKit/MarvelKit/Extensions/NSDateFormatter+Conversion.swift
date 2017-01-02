923edb2b352f1a174e55fc1376911c5370402e92
Carsten Könemann <cargath@gmail.com>
2017-01-02 20:26:57 +0100

    Implement ISO8601 date formatter + formatting helpers

--- MarvelKit/MarvelKit/Extensions/NSDateFormatter+Conversion.swift
+++ MarvelKit/MarvelKit/Extensions/NSDateFormatter+Conversion.swift
@@ -8,14 +8,38 @@
 
 import Foundation
 
+extension String {
+
+    static var iso8601DateFormat: String {
+        return "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+    }
+
+    static var simpleDateFormat: String {
+        return "yyyy-MM-dd"
+    }
+
+}
+
 extension DateFormatter {
 
-    static var marvelDateFormatter: DateFormatter {
+    static var iso8601Formatter: DateFormatter {
+
+        let iso8601Formatter = DateFormatter()
+
+        iso8601Formatter.calendar = Calendar(identifier: .iso8601)
+        iso8601Formatter.dateFormat = .iso8601DateFormat
+        iso8601Formatter.locale = Locale(identifier: "en_US_POSIX")
+        iso8601Formatter.timeZone = TimeZone(secondsFromGMT: 0)
+
+        return iso8601Formatter
+    }
+
+    static var simpleDateFormatter: DateFormatter {
 
         let dateFormatter = DateFormatter()
 
         dateFormatter.calendar = Calendar(identifier: Calendar.Identifier.gregorian)
-        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.dateFormat = .simpleDateFormat
         dateFormatter.locale = Locale.current
         dateFormatter.timeZone = TimeZone.autoupdatingCurrent
 
@@ -26,16 +50,24 @@ extension DateFormatter {
 
 extension Date {
 
+    var iso8601String: String {
+        return DateFormatter.iso8601Formatter.string(from: self)
+    }
+
     var string: String {
-        return DateFormatter.marvelDateFormatter.string(from: self)
+        return DateFormatter.simpleDateFormatter.string(from: self)
     }
 
 }
 
 extension String {
 
+    var iso8601Date: Date? {
+        return DateFormatter.iso8601Formatter.date(from: self)
+    }
+
     var date: Date? {
-        return DateFormatter.marvelDateFormatter.date(from: self)
+        return DateFormatter.simpleDateFormatter.date(from: self)
     }
     
 }
